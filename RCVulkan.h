f392afe47a0bb276e2f931c42b859e19681a53ba
Rolando Caloca <rcaloca@gmail.com>
2018-11-01 10:48:26 -0400

    Device

--- RCVulkan.h
+++ RCVulkan.h
@@ -1,12 +1,28 @@
 #pragma once
 
 #include "../RCUtils/RCUtilsBase.h"
-#include "../volk/volk.h"
+
+/*
 #define WIN32_LEAN_AND_MEAN
 #include <windows.h>
 
-#define VERIFY_VKRESULT(x)	if ((x) != VK_SUCCESS) { ::OutputDebugStringA(#x); ::OutputDebugStringA("\n"); check(0);}
+#define VK_NO_PROTOTYPES
+#ifdef _WIN32
+#include <vulkan/vulkan_core.h>
+#include <vulkan/vulkan_win32.h>
+#endif
 
+#ifdef APIENTRY
+#undef APIENTRY
+#endif
+*/
+
+#include "../volk/volk.h"
+
+#include <GLFW/glfw3native.h>
+
+
+#define VERIFY_VKRESULT(x)	if ((x) != VK_SUCCESS) { ::OutputDebugStringA(#x); ::OutputDebugStringA("\n"); check(0);}
 
 template <typename T>
 inline void ZeroVulkanMem(T& VulkanStruct, VkStructureType Type)
@@ -22,7 +38,24 @@ struct SVulkanInstance
 	VkInstance Instance = VK_NULL_HANDLE;
 	VkDebugReportCallbackEXT DebugReportCallback = nullptr;
 
-	void Init()
+	std::vector<VkPhysicalDevice> DiscreteDevices;
+	std::vector<VkPhysicalDevice> IntegratedDevices;
+
+	struct FDevice
+	{
+		VkDevice Device = VK_NULL_HANDLE;
+		VkPhysicalDevice PhysicalDevice = VK_NULL_HANDLE;
+		VkPhysicalDeviceProperties Props;
+		uint32 GfxQueue = VK_QUEUE_FAMILY_IGNORED;
+		uint32 ComputeQueue = VK_QUEUE_FAMILY_IGNORED;
+		uint32 TransferQueue = VK_QUEUE_FAMILY_IGNORED;
+	};
+
+	std::map<VkPhysicalDevice, FDevice> Devices;
+
+	VkPhysicalDevice PhysicalDevice = VK_NULL_HANDLE;
+
+	void Init(GLFWwindow* Window)
 	{
 		VERIFY_VKRESULT(volkInitialize());
 
@@ -31,11 +64,163 @@ struct SVulkanInstance
 		volkLoadInstance(Instance);
 
 		InitDebugCallback();
+
+		SetupDevices(Window);
+	}
+
+	static uint32 FindQueue(VkPhysicalDevice PhysicalDevice, VkQueueFlagBits QueueFlag)
+	{
+		uint32 NumQueues = 0;
+		vkGetPhysicalDeviceQueueFamilyProperties(PhysicalDevice, &NumQueues, 0);
+
+		std::vector<VkQueueFamilyProperties> QueueProps(NumQueues);
+		vkGetPhysicalDeviceQueueFamilyProperties(PhysicalDevice, &NumQueues, QueueProps.data());
+
+		for (uint32 i = 0; i < NumQueues; ++i)
+		{
+			if (QueueProps[i].queueFlags & QueueFlag)
+			{
+				return i;
+			}
+		}
+
+		return VK_QUEUE_FAMILY_IGNORED;
+	}
+
+	void GetPhysicalDevices()
+	{
+		uint32 Count = 0;
+		VERIFY_VKRESULT(vkEnumeratePhysicalDevices(Instance, &Count, nullptr));
+
+		std::vector<VkPhysicalDevice> PhysicalDevices(Count);
+		VERIFY_VKRESULT(vkEnumeratePhysicalDevices(Instance, &Count, PhysicalDevices.data()));
+
+		for (auto& PD : PhysicalDevices)
+		{
+			VkPhysicalDeviceProperties Props;
+			vkGetPhysicalDeviceProperties(PD, &Props);
+
+			uint32 GfxQueue = FindQueue(PD, VK_QUEUE_GRAPHICS_BIT);
+			uint32 ComputeQueue = FindQueue(PD, VK_QUEUE_COMPUTE_BIT);
+			uint32 TransferQueue = FindQueue(PD, VK_QUEUE_TRANSFER_BIT);
+			if (GfxQueue == VK_QUEUE_FAMILY_IGNORED)
+			{
+				continue;
+			}
+
+#if defined(VK_USE_PLATFORM_WIN32_KHR) && VK_USE_PLATFORM_WIN32_KHR
+			if (!vkGetPhysicalDeviceWin32PresentationSupportKHR(PD, GfxQueue))
+			{
+				continue;
+			}
+#endif
+			if (Props.apiVersion < VK_API_VERSION_1_1)
+			{
+				continue;
+			}
+
+			if (Props.deviceType == VK_PHYSICAL_DEVICE_TYPE_DISCRETE_GPU)
+			{
+				DiscreteDevices.push_back(PD);
+			}
+			else if (Props.deviceType == VK_PHYSICAL_DEVICE_TYPE_INTEGRATED_GPU)
+			{
+				IntegratedDevices.push_back(PD);
+			}
+			else
+			{
+				// What is this? :)
+				check(0);
+			}
+
+			auto& Device = Devices[PD];
+			Device.PhysicalDevice = PD;
+			Device.Props = Props;
+			Device.ComputeQueue = ComputeQueue;
+			Device.GfxQueue = GfxQueue;
+			Device.TransferQueue = TransferQueue;
+		}
+	}
+
+	static void CreateDevice(FDevice& Device)
+	{
+		uint32 Count = 0;
+		VERIFY_VKRESULT(vkEnumerateDeviceExtensionProperties(Device.PhysicalDevice, nullptr, &Count, nullptr));
+		std::vector<VkExtensionProperties> ExtensionProperties(Count);
+		VERIFY_VKRESULT(vkEnumerateDeviceExtensionProperties(Device.PhysicalDevice, nullptr, &Count, ExtensionProperties.data()));
+
+		check(Device.Props.limits.timestampComputeAndGraphics);
+
+		float Priorities[1] = {1.0f};
+
+		const char* DeviceExtensions[] =
+		{
+			VK_KHR_SWAPCHAIN_EXTENSION_NAME,
+/*
+			VK_KHR_PUSH_DESCRIPTOR_EXTENSION_NAME,
+			VK_KHR_16BIT_STORAGE_EXTENSION_NAME,
+			VK_KHR_8BIT_STORAGE_EXTENSION_NAME,
+*/
+		};
+
+		VerifyExtensions(ExtensionProperties, DeviceExtensions);
+
+		VkDeviceQueueCreateInfo QueueInfo;
+		ZeroVulkanMem(QueueInfo, VK_STRUCTURE_TYPE_DEVICE_QUEUE_CREATE_INFO);
+		QueueInfo.queueFamilyIndex =Device.GfxQueue;
+		QueueInfo.queueCount = 1;
+		QueueInfo.pQueuePriorities = Priorities;
+
+		VkDeviceCreateInfo CreateInfo;
+		ZeroVulkanMem(CreateInfo, VK_STRUCTURE_TYPE_DEVICE_CREATE_INFO);
+		CreateInfo.queueCreateInfoCount = 1;
+		CreateInfo.pQueueCreateInfos = &QueueInfo;
+		CreateInfo.ppEnabledExtensionNames = DeviceExtensions;
+		CreateInfo.enabledExtensionCount = sizeof(DeviceExtensions) / sizeof(DeviceExtensions[0]);
+
+		VERIFY_VKRESULT(vkCreateDevice(Device.PhysicalDevice, &CreateInfo, nullptr, &Device.Device));
+	}
+
+	void SetupSwapchain(FDevice& Device, GLFWwindow* Window)
+	{
+#if defined(VK_USE_PLATFORM_WIN32_KHR)
+		VkWin32SurfaceCreateInfoKHR CreateInfo;
+		ZeroVulkanMem(CreateInfo, VK_STRUCTURE_TYPE_WIN32_SURFACE_CREATE_INFO_KHR);
+		CreateInfo.hinstance = GetModuleHandle(0);
+		CreateInfo.hwnd = glfwGetWin32Window(Window);
+
+		VkSurfaceKHR Surface = VK_NULL_HANDLE;
+		VERIFY_VKRESULT(vkCreateWin32SurfaceKHR(Instance, &CreateInfo, 0, &Surface));
+#endif
+	}
+
+	void SetupDevices(GLFWwindow* Window)
+	{
+		GetPhysicalDevices();
+		check(!DiscreteDevices.empty() || !IntegratedDevices.empty());
+
+		if (!DiscreteDevices.empty())
+		{
+			PhysicalDevice = DiscreteDevices.front();
+		}
+		else
+		{
+			PhysicalDevice = IntegratedDevices.front();
+		}
+
+		CreateDevice(Devices[PhysicalDevice]);
+		SetupSwapchain(Devices[PhysicalDevice], Window);
 	}
 
 	static VkBool32 DebugReport(VkDebugReportFlagsEXT Flags, VkDebugReportObjectTypeEXT ObjectType, uint64_t Object,
 		size_t Location, int32_t MessageCode, const char* LayerPrefix, const char* Message, void* UserData)
 	{
+		std::string s = (Flags & VK_DEBUG_REPORT_ERROR_BIT_EXT) ? "[Error]" : "";
+		s += (Flags & VK_DEBUG_REPORT_WARNING_BIT_EXT) ? "[Warning]" : "";
+		s += (Flags & VK_DEBUG_REPORT_PERFORMANCE_WARNING_BIT_EXT) ? "[Perf]" : "";
+		s += " ";
+		s += Message;
+		::OutputDebugStringA(s.c_str());
 		return VK_FALSE;
 	}
 
@@ -97,21 +282,18 @@ struct SVulkanInstance
 	{
 		VkInstanceCreateInfo Info;
 		ZeroVulkanMem(Info, VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO);
-		//Info.
 
 		uint32_t Count = 0;
 		VERIFY_VKRESULT(vkEnumerateInstanceLayerProperties(&Count, nullptr));
 
-		std::vector<VkLayerProperties> LayerProperties;
-		LayerProperties.resize(Count);
-		VERIFY_VKRESULT(vkEnumerateInstanceLayerProperties(&Count, &LayerProperties[0]));
+		std::vector<VkLayerProperties> LayerProperties(Count);
+		VERIFY_VKRESULT(vkEnumerateInstanceLayerProperties(&Count, LayerProperties.data()));
 
 		Count = 0;
 		VERIFY_VKRESULT(vkEnumerateInstanceExtensionProperties(nullptr, &Count, nullptr));
 
-		std::vector<VkExtensionProperties> ExtensionProperties;
-		ExtensionProperties.resize(Count);
-		VERIFY_VKRESULT(vkEnumerateInstanceExtensionProperties(nullptr, &Count, &ExtensionProperties[0]));
+		std::vector<VkExtensionProperties> ExtensionProperties(Count);
+		VERIFY_VKRESULT(vkEnumerateInstanceExtensionProperties(nullptr, &Count, ExtensionProperties.data()));
 
 		const char* Layers[] =
 		{
@@ -125,6 +307,9 @@ struct SVulkanInstance
 		{
 			VK_KHR_SURFACE_EXTENSION_NAME,
 			VK_EXT_DEBUG_REPORT_EXTENSION_NAME,
+#if defined(VK_USE_PLATFORM_WIN32_KHR) && VK_USE_PLATFORM_WIN32_KHR
+			VK_KHR_WIN32_SURFACE_EXTENSION_NAME,
+#endif
 		};
 		VerifyExtensions(ExtensionProperties, Extensions);
 		Info.ppEnabledExtensionNames = Extensions;
