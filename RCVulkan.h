effae256f7e70604d41369ad5684ba8150fd81dc
Rolando Caloca <rcaloca@gmail.com>
2018-11-10 23:38:50 -0500

    Move some classes out

--- RCVulkan.h
+++ RCVulkan.h
@@ -7,9 +7,14 @@
 #include <algorithm>
 #include <atomic>
 #include <sstream>
-/*
+#include <direct.h>
+
 #define WIN32_LEAN_AND_MEAN
 #include <windows.h>
+
+#include "../RCUtils/RCUtilsFile.h"
+
+/*
 
 #define VK_NO_PROTOTYPES
 #ifdef _WIN32
@@ -1192,6 +1197,399 @@ struct SVulkan
 
 		vkDestroyInstance(Instance, nullptr);
 		Instance = VK_NULL_HANDLE;
+	}
+};
+
+struct FShaderInfo
+{
+	enum class EStage
+	{
+		Unknown = -1,
+		Vertex,
+		Pixel,
+
+		Compute = 16,
+	};
+	EStage Stage = EStage::Unknown;
+	std::string EntryPoint;
+	std::string SourceFile;
+	std::string BinaryFile;
+	std::string AsmFile;
+	SVulkan::FShader* Shader = nullptr;
+
+	~FShaderInfo()
+	{
+		check(!Shader);
+	}
+
+	inline bool NeedsRecompiling() const
+	{
+		return RCUtils::IsNewerThan(SourceFile, BinaryFile);
+	}
+};
+
+struct FShaderLibrary
+{
+	std::vector<FShaderInfo*> ShaderInfos;
+
+	VkDevice Device =  VK_NULL_HANDLE;
+	void Init(VkDevice InDevice)
+	{
+		Device = InDevice;
+	}
+
+	FShaderInfo* RegisterShader(const char* OriginalFilename, const char* EntryPoint, FShaderInfo::EStage Stage)
+	{
+		FShaderInfo* Info = new FShaderInfo;
+		Info->EntryPoint = EntryPoint;
+		Info->Stage = Stage;
+
+		std::string RootDir;
+		std::string BaseFilename;
+		std::string Extension = RCUtils::SplitPath(OriginalFilename, RootDir, BaseFilename, false);
+
+		std::string OutDir = RCUtils::MakePath(RootDir, "out");
+		_mkdir(OutDir.c_str());
+
+		Info->SourceFile = RCUtils::MakePath(RootDir, BaseFilename + "." + Extension);
+		Info->BinaryFile = RCUtils::MakePath(OutDir, BaseFilename + "." + Info->EntryPoint + ".spv");
+		Info->AsmFile = RCUtils::MakePath(OutDir, BaseFilename + "." + Info->EntryPoint + ".spvasm");
+
+		ShaderInfos.push_back(Info);
+
+		return Info;
+	}
+
+	void RecompileShaders()
+	{
+		for (auto* Info : ShaderInfos)
+		{
+			if (Info->NeedsRecompiling())
+			{
+				DoCompileFromSource(Info);
+			}
+			else if (!Info->Shader)
+			{
+				DoCompileFromBinary(Info);
+			}
+		}
+	}
+
+	static std::string GetGlslangCommandLine()
+	{
+		std::string Out;
+		char Glslang[MAX_PATH];
+		char SDKDir[MAX_PATH];
+		::GetEnvironmentVariableA("VULKAN_SDK", SDKDir, MAX_PATH - 1);
+		sprintf_s(Glslang, "%s\\Bin\\glslangValidator.exe", SDKDir);
+		Out = Glslang;
+		Out += " -V -r -l -H -D --hlsl-iomap --auto-map-bindings";
+		return Out;
+	}
+
+	static std::string GetStageName(FShaderInfo::EStage Stage)
+	{
+		switch (Stage)
+		{
+		case FShaderInfo::EStage::Compute:	return "comp";
+		case FShaderInfo::EStage::Vertex:	return "vert";
+		case FShaderInfo::EStage::Pixel:	return "frag";
+		default:
+			break;
+		}
+
+		return "INVALID";
+	}
+
+	bool CreateShader(FShaderInfo* Info, std::vector<char>& Data)
+	{
+		Info->Shader = new SVulkan::FShader;
+		Info->Shader->SpirV = Data;
+		return Info->Shader->Create(Device);
+	}
+
+	bool DoCompileFromBinary(FShaderInfo* Info)
+	{
+		std::vector<char> File = RCUtils::LoadFileToArray(Info->BinaryFile.c_str());
+		if (File.empty())
+		{
+			check(0);
+			return false;
+		}
+
+		check(!Info->Shader);
+		//#todo: Destroy old; sync with rendering
+		//if (Info.Shader)
+		//{
+		//	ShadersToDestroy.push_back(Info.Shader);
+		//}
+		return CreateShader(Info, File);
+	}
+
+	bool DoCompileFromSource(FShaderInfo* Info)
+	{
+		static const std::string GlslangProlog = GetGlslangCommandLine();
+
+		std::string Compile = GlslangProlog;
+		Compile += " -e " + Info->EntryPoint;
+		Compile += " -o " + RCUtils::AddQuotes(Info->BinaryFile);
+		Compile += " -S " + GetStageName(Info->Stage);
+		Compile += " " + RCUtils::AddQuotes(Info->SourceFile);
+		Compile += " > " + RCUtils::AddQuotes(Info->AsmFile);
+		int ReturnCode = system(Compile.c_str());
+		if (ReturnCode)
+		{
+			std::vector<char> File = RCUtils::LoadFileToArray(Info->AsmFile.c_str());
+			if (File.empty())
+			{
+				std::string Error = "Compile error: No output for file ";
+				Error += Info->SourceFile;
+				::OutputDebugStringA(Error.c_str());
+			}
+			else
+			{
+				std::string FileString = &File[0];
+				FileString.resize(File.size());
+				std::string Error = "Compile error:\n";
+				Error += FileString;
+				Error += "\n";
+				::OutputDebugStringA(Error.c_str());
+
+				int DialogResult = ::MessageBoxA(nullptr, Error.c_str(), Info->SourceFile.c_str(), MB_CANCELTRYCONTINUE);
+				if (DialogResult == IDTRYAGAIN)
+				{
+					return DoCompileFromSource(Info);
+				}
+			}
+
+			return false;
+		}
+
+		return DoCompileFromBinary(Info);
+	}
+
+	void DestroyShaders()
+	{
+		//#todo-rco: Sync with GPU
+		for (auto* Info : ShaderInfos)
+		{
+			delete Info->Shader;
+			Info->Shader = nullptr;
+		}
+	}
+
+	void Destroy()
+	{
+		DestroyShaders();
+
+		for (auto* Info : ShaderInfos)
+		{
+			delete Info;
+		}
+		ShaderInfos.clear();
+	}
+};
+
+struct FRenderTargetCache
+{
+	std::map<uint64, SVulkan::FRenderPass*> RenderPasses;
+	std::vector<SVulkan::FFramebuffer*> Framebuffers;
+
+	VkDevice Device =  VK_NULL_HANDLE;
+	void Init(VkDevice InDevice)
+	{
+		Device = InDevice;
+	}
+
+	SVulkan::FRenderPass* GetOrCreateRenderPass(VkFormat Format, VkAttachmentLoadOp LoadOp, VkAttachmentStoreOp StoreOp)
+	{
+		uint64 Key = (uint64)Format << (uint64)32;
+		Key = Key | ((uint64)LoadOp << 24);
+		Key = Key | ((uint64)StoreOp << 16);
+		auto Found = RenderPasses.find(Key);
+		if (Found != RenderPasses.end())
+		{
+			return Found->second;
+		}
+
+		SVulkan::FRenderPass* RenderPass = new SVulkan::FRenderPass();
+		RenderPass->Create(Device, Format, LoadOp, StoreOp);
+		RenderPasses[Key] = RenderPass;
+		return RenderPass;
+	}
+
+	SVulkan::FFramebuffer* GetOrCreateFrameBuffer(SVulkan::FSwapchain* Swapchain, VkAttachmentLoadOp LoadOp, VkAttachmentStoreOp StoreOp)
+	{
+		if (Swapchain->ImageIndex < Framebuffers.size())
+		{
+			return Framebuffers[Swapchain->ImageIndex];
+		}
+
+		SVulkan::FRenderPass* RenderPass = GetOrCreateRenderPass(Swapchain->Format, LoadOp, StoreOp);
+
+		SVulkan::FFramebuffer* FB = new SVulkan::FFramebuffer();
+		VkViewport Viewport = Swapchain->GetViewport();
+		FB->Create(Device, Swapchain->ImageViews[Swapchain->ImageIndex], (uint32)Viewport.width, (uint32)Viewport.height, RenderPass);
+		Framebuffers.push_back(FB);
+		return FB;
+	}
+
+	void Destroy()
+	{
+		for (auto* FB : Framebuffers)
+		{
+			FB->Destroy();
+			delete FB;
+		}
+		Framebuffers.clear();
+
+		for (auto Pair : RenderPasses)
+		{
+			Pair.second->Destroy();
+			delete Pair.second;
+		}
+		RenderPasses.clear();
+	}
+};
+
+struct FPSOCache
+{
+	std::map<SVulkan::FShader*, std::map<SVulkan::FShader*, VkPipelineLayout>> PipelineLayouts;
+	std::vector<VkPipeline> PSOs;
+
+	VkDevice Device =  VK_NULL_HANDLE;
+	void Init(VkDevice InDevice)
+	{
+		Device = InDevice;
+	}
+
+	VkPipelineLayout GetOrCreatePipelineLayout(SVulkan::FShader* VS, SVulkan::FShader* PS)
+	{
+		auto& VSList = PipelineLayouts[VS];
+		auto Found = VSList.find(PS);
+		if (Found != VSList.end())
+		{
+			return Found->second;
+		}
+
+		VkPipelineLayoutCreateInfo Info;
+		ZeroVulkanMem(Info, VK_STRUCTURE_TYPE_PIPELINE_LAYOUT_CREATE_INFO);
+
+		VkPipelineLayout PipelineLayout = VK_NULL_HANDLE;
+		VERIFY_VKRESULT(vkCreatePipelineLayout(Device, &Info, nullptr, &PipelineLayout));
+
+		VSList[PS] = PipelineLayout;
+
+		return PipelineLayout;
+	}
+
+	template <typename TFunction>
+	VkPipeline CreatePSO(FShaderInfo* VS, FShaderInfo* PS, SVulkan::FRenderPass* RenderPass, TFunction Callback)
+	{
+		check(VS->Shader && VS->Shader->ShaderModule);
+		if (PS)
+		{
+			check(PS->Shader && PS->Shader->ShaderModule);
+		}
+
+		VkGraphicsPipelineCreateInfo GfxPipelineInfo;
+		ZeroVulkanMem(GfxPipelineInfo, VK_STRUCTURE_TYPE_GRAPHICS_PIPELINE_CREATE_INFO);
+
+		GfxPipelineInfo.renderPass = RenderPass->RenderPass;
+
+		VkPipelineInputAssemblyStateCreateInfo IAInfo;
+		ZeroVulkanMem(IAInfo, VK_STRUCTURE_TYPE_PIPELINE_INPUT_ASSEMBLY_STATE_CREATE_INFO);
+		IAInfo.topology = VK_PRIMITIVE_TOPOLOGY_TRIANGLE_LIST;
+		GfxPipelineInfo.pInputAssemblyState = &IAInfo;
+
+		VkPipelineVertexInputStateCreateInfo VertexInputInfo;
+		ZeroVulkanMem(VertexInputInfo, VK_STRUCTURE_TYPE_PIPELINE_VERTEX_INPUT_STATE_CREATE_INFO);
+		GfxPipelineInfo.pVertexInputState = &VertexInputInfo;
+
+		VkPipelineShaderStageCreateInfo StageInfos[2];
+		ZeroVulkanMem(StageInfos[0], VK_STRUCTURE_TYPE_PIPELINE_SHADER_STAGE_CREATE_INFO);
+		StageInfos[0].stage = VK_SHADER_STAGE_VERTEX_BIT;
+		StageInfos[0].module = VS->Shader->ShaderModule;
+		StageInfos[0].pName = VS->EntryPoint.c_str();
+		GfxPipelineInfo.stageCount = 1;
+
+		if (PS)
+		{
+			ZeroVulkanMem(StageInfos[1], VK_STRUCTURE_TYPE_PIPELINE_SHADER_STAGE_CREATE_INFO);
+			StageInfos[1].stage = VK_SHADER_STAGE_FRAGMENT_BIT;
+			StageInfos[1].module = PS->Shader->ShaderModule;
+			StageInfos[1].pName = PS->EntryPoint.c_str();
+			++GfxPipelineInfo.stageCount;
+		}
+		GfxPipelineInfo.pStages = StageInfos;
+
+		VkPipelineLayout PipelineLayout = GetOrCreatePipelineLayout(VS->Shader, PS ? PS->Shader : nullptr);
+
+		VkPipelineRasterizationStateCreateInfo RasterizerInfo;
+		ZeroVulkanMem(RasterizerInfo, VK_STRUCTURE_TYPE_PIPELINE_RASTERIZATION_STATE_CREATE_INFO);
+		RasterizerInfo.cullMode = VK_CULL_MODE_NONE;
+		RasterizerInfo.lineWidth = 1.0f;
+		GfxPipelineInfo.pRasterizationState = &RasterizerInfo;
+
+		VkPipelineColorBlendAttachmentState BlendAttachState;
+		ZeroMem(BlendAttachState);
+		BlendAttachState.colorWriteMask = 0xf;
+		VkPipelineColorBlendStateCreateInfo BlendInfo;
+		ZeroVulkanMem(BlendInfo, VK_STRUCTURE_TYPE_PIPELINE_COLOR_BLEND_STATE_CREATE_INFO);
+		BlendInfo.attachmentCount = 1;
+		BlendInfo.pAttachments = &BlendAttachState;
+		GfxPipelineInfo.pColorBlendState = &BlendInfo;
+
+		VkViewport Viewport;
+		ZeroMem(Viewport);
+		Viewport.width = 1280;
+		Viewport.height = 960;
+		Viewport.maxDepth = 1;
+		VkRect2D Scissor;
+		Scissor.extent.width = (uint32)Viewport.width;
+		Scissor.extent.height = (uint32)Viewport.height;
+
+		VkPipelineViewportStateCreateInfo ViewportState;
+		ZeroVulkanMem(ViewportState, VK_STRUCTURE_TYPE_PIPELINE_VIEWPORT_STATE_CREATE_INFO);
+		GfxPipelineInfo.pViewportState = &ViewportState;
+
+		GfxPipelineInfo.layout = PipelineLayout;
+
+		VkPipelineDepthStencilStateCreateInfo DepthStateInfo;
+		ZeroVulkanMem(DepthStateInfo, VK_STRUCTURE_TYPE_PIPELINE_DEPTH_STENCIL_STATE_CREATE_INFO);
+		DepthStateInfo.front = DepthStateInfo.back;
+		GfxPipelineInfo.pDepthStencilState = &DepthStateInfo;
+
+		VkPipelineMultisampleStateCreateInfo MSInfo;
+		ZeroVulkanMem(MSInfo, VK_STRUCTURE_TYPE_PIPELINE_MULTISAMPLE_STATE_CREATE_INFO);
+		MSInfo.rasterizationSamples = VK_SAMPLE_COUNT_1_BIT;
+		GfxPipelineInfo.pMultisampleState = &MSInfo;
+
+		Callback(GfxPipelineInfo);
+
+		VkPipeline Pipeline;
+		VERIFY_VKRESULT(vkCreateGraphicsPipelines(Device, VK_NULL_HANDLE, 1, &GfxPipelineInfo, nullptr, &Pipeline));
+		PSOs.push_back(Pipeline);
+		return Pipeline;
+	}
+
+	void Destroy()
+	{
+		for (auto VSPair : PipelineLayouts)
+		{
+			for (auto PSPair : VSPair.second)
+			{
+				vkDestroyPipelineLayout(Device, PSPair.second, nullptr);
+			}
+		}
+
+		PipelineLayouts.clear();
+
+		for (auto* PSO : PSOs)
+		{
+			vkDestroyPipeline(Device, PSO, nullptr);
+		}
+		PSOs.clear();
 	}
 };
 
